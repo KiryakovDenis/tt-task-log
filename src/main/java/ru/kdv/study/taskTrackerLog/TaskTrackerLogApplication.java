5c5b281987158ad71842e7915ae59e2721124814
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-04-12 12:59:47 +0500

    Initital commit

---
+++ src/main/java/ru/kdv/study/taskTrackerLog/TaskTrackerLogApplication.java
@@ -0,0 +1,11 @@
+package ru.kdv.study.taskTrackerLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerLogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerLogApplication.class, args);
+    }
+}
